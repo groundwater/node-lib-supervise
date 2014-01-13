0a3c2d4f55bf6a0ed2d1d94f8659ced4c8598519
Jacob Groundwater <jacob@newrelic.com>
2014-01-12 21:34:34 -0800

    some stuff I thought was a good idea

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var spawn = require('child_process').spawn;
 var PIPE = 'pipe';
 
 function Job(job) {
-  this._exec = job.exec;
-  this._args = job.args || [];
-  this._cwd  = job.cwd  || process.cwd();
-  this._envs = job.envs || process.env;
+  this.exec = job.exec;
+  this.args = job.args || [];
+  this.cwd  = job.cwd  || process.cwd();
+  this.envs = job.envs || process.env;
 }
 
 Job.prototype._proc = function _proc() {
-  var exec = this._exec;
-  var args = this._args;
+  var exec = this.exec;
+  var args = this.args;
   var opts = {
-    cwd   : this._cwd,
+    cwd   : this.cwd,
     stdio : PIPE
   };
   var proc = spawn(exec, args, opts);
@@ -21,7 +21,10 @@ Job.prototype._proc = function _proc() {
   return proc;
 }
 
-// start a process that will be restarted if it dies
+// start a process
+// this function is actually stateless,
+// calling it with two separate event emitters will
+// start two disjoint processes
 Job.prototype.start = function start(ee) {
   var self = this;
   var proc = this._proc();
@@ -31,14 +34,16 @@ Job.prototype.start = function start(ee) {
   // remember that this happens synchronously
   ee.emit('run', proc);
 
-  // on exit, we need to decide about restarting or quitting
-  // the restart logic is entirely governed by this.restart
-  // users can feel free to replace this.restart as they like
+  // the decision to restart on death or not is delegated
+  // the the event emitter. If you want to restart, it's easy:
+  // just call job.start(ee) again
   proc.on('exit', function (code, signal) {
     ee.emit('die', code, signal, self);
   });
 };
 
+// if the process dies with non-zero exit code,
+// wait 100ms and try again to avoid peggin the CPU
 function default_retry(code, signal, job) {
   var ee = this;
   if (code!==0) setTimeout(function () {
@@ -47,15 +52,26 @@ function default_retry(code, signal, job) {
   else ee.emit('end');
 }
 
-module.exports = function (stanza, retry) {
+module.exports = function (stanza, option_retry) {
+  var retry = option_retry || default_retry;
+
+  module.exports.start(stanza, retry);
+}
+
+module.exports.start = function (stanza, retry) {
   var ee  = new (require('events').EventEmitter)();
   var job = new Job(stanza);
   
-  ee.on('die', retry || default_retry);
+  ee.on('die', retry);
+
+  // nextTick the start event because the returned
+  // event emitter needs to have listeners attached
+  // otherwise we're going to miss catching the first process
   process.nextTick(function () {
     job.start(ee);
   });
 
   return ee;
 }
+
 module.exports.Job = Job;
