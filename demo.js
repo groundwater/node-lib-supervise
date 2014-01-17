3ab687e94a189a4b4700038befbce9f31e42800f
Jacob Groundwater <jacob@newrelic.com>
2014-01-16 22:45:04 -0800

    clarify demo

--- demo.js
+++ demo.js
@@ -2,27 +2,27 @@ var supervise = require('./index.js');
 
 var fail = 0;
 
-var ee = supervise({
+var job = supervise({
   exec : 'node',
   args : ['server.js']
 });
 
-ee.on('run', function start(proc) {
+job.on('run', function start(proc) {
   proc.stdout.pipe(process.stdout);
   proc.stderr.pipe(process.stdout);
   console.log("----> job", proc.pid);
 });
 
-ee.on('die', function fault(code, signal) {
+job.on('die', function fault(code, signal) {
   // stop trapping after 5 failures
-  if (++fail > 4) ee.retry = null;
+  if (++fail > 4) job.retry = null;
   console.log("----> die", fail);
 });
 
-ee.on('error', function (err) {
+job.on('error', function (err) {
   console.log("----> error", err);
 });
 
-ee.on('end', function () {
+job.on('end', function () {
   console.log('END');
 });
--- server.js
+++ server.js
@@ -1,1 +1,2 @@
 console.log("Hello World");
+throw 1;
